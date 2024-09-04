90b73111c91f1e10d3c245fc73bcb13ea961bf55
SaimRazaAly <saimrazateach2010@gmail.com>
2024-09-04 01:10:26 -0700

    Fix the bugs and make the hamburger work sucessfully

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 // searchinga all doim element
-let openit = document.querySelector('#openit')
-let unorder = document.querySelector('#unorder')
-let navbar = document.querySelector('#navbar')
-let header = document.querySelector('header')
+let openit = document.querySelector("#openit");
+let closeit = document.querySelector("#close");
+let unorder = document.querySelector("#unorder");
+let navbar = document.querySelector("#navbar");
+let header = document.querySelector("header");
 
-openit.onclick = (()=>{
- header.classList.toggle('h-column')
- unorder.classList.toggle('block')
- if (unorder.classList.contains('block')) {
-   openit.innerText = 'X'
-  
- }
- else  {
-   openit.innerText = '='
-  
- }
-})
+openit.onclick = () => {
+  header.classList.add("h-column");
+  unorder.classList.add("block");
+  navbar.style.padding='10px'
+openit.style.display='none'
+
+}
+
+closeit.onclick = () => {
+  header.classList.remove("h-column");
+  unorder.classList.remove("block");
+  openit.style.display='block'
+};
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    header.style.background = "#ffff";
+    header.style.color = "black";
+  } else {
+    header.style.background = "transparent";
+    header.style.color = "#fffff";
+  }
+};
